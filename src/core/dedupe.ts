12d40592026df2d3f835d2c192ddecbd624ff9e6
Bailey Pearson <bailey.pearson@vanderbilt.edu>
2021-04-24 09:15:29 -0400

    test?

--- src/core/dedupe.ts
+++ src/core/dedupe.ts
@@ -1,16 +1,39 @@
-
+function* _dedupe<T>(iter: Iterable<T>): Generator<T> {
+  const values = new Set()
+  for (const value of iter) {
+    if (!values.has(value)) {
+      values.add(value)
+      yield value
+    }
+  }
+}
 
 /**
  * dedupe
- * 
+ *
+ * A curried verison of the dedupe operator.  This overload takes no parameters
+ * and returns an operator that can be used to filter out any duplicate values in
+ * an iterable.
+ *
  * @returns An iterable that contains only the first of each distinct value.
  */
-export function* dedupe<T>(iter: Iterable<T>): Generator<T> {
-    const values = new Set()
-    for (const value of iter) {
-        if (!values.has(value)) {
-            values.add(value)
-            yield value
-        }
-    }
+export function dedupe<T>(): (arg0: Iterable<T>) => Generator<T>
+
+/**
+ * dedupe
+ *
+ * @returns An iterable that contains only the first of each distinct value.
+ */
+export function dedupe<T>(iter: Iterable<T>): Generator<T>
+
+export function dedupe<T>(
+  iter?: Iterable<T>
+): Generator<T> | ((arg0: Iterable<T>) => Generator<T>) {
+  if (iter === undefined) {
+    return _dedupe
+  }
+
+  return (function*() {
+    yield* _dedupe(iter)
+  })()
 }
--- test/core/dedupe.spec.ts
+++ test/core/dedupe.spec.ts
@@ -1,28 +1,61 @@
-import { dedupe } from '../../src/core/dedupe';
-import { into } from '../../src/utility/into';
+import { dedupe } from '../../src/core/dedupe'
+import { into } from '../../src/utility/into'
 
 describe('dedupe tests', () => {
+  describe('non-curried tests', () => {
     it('should yield nothing on empty iterable', () => {
-        const source = [] as number[]
-        const result = dedupe(source)
-        expect(into('array', result)).toEqual([])
+      const source = [] as number[]
+      const result = dedupe(source)
+      expect(into('array', result)).toEqual([])
     })
 
     it('should not modify an iterable if there are no duplicates', () => {
-        const source = [1,2,3]
-        const result = dedupe(source)
-        expect(into('array', result)).toEqual([1,2,3])
+      const source = [1, 2, 3]
+      const result = dedupe(source)
+      expect(into('array', result)).toEqual([1, 2, 3])
     })
 
     it('should get rid of consecutive duplicates', () => {
-        const source = [1, 1, 2, 2, 3, 3]
-        const result = dedupe(source)
-        expect(into('array', result)).toEqual([1,2,3])
+      const source = [1, 1, 2, 2, 3, 3]
+      const result = dedupe(source)
+      expect(into('array', result)).toEqual([1, 2, 3])
     })
 
     it('should get rid of any non-consecutive duplicates', () => {
-        const source = [1, 2, 3, 1, 2, 3]
-        const result = dedupe(source)
-        expect(into('array', result)).toEqual([1,2,3])
+      const source = [1, 2, 3, 1, 2, 3]
+      const result = dedupe(source)
+      expect(into('array', result)).toEqual([1, 2, 3])
     })
+  })
+
+  describe('curried tests', () => {
+    it('should yield nothing on empty iterable', () => {
+      const deduper = dedupe()
+      console.error(deduper)
+      const source = [] as number[]
+      const result = deduper(source)
+      expect(into('array', result)).toEqual([])
+    })
+
+    it('should not modify an iterable if there are no duplicates', () => {
+      const deduper = dedupe()
+      const source = [1, 2, 3]
+      const result = deduper(source)
+      expect(into('array', result)).toEqual([1, 2, 3])
+    })
+
+    it('should get rid of consecutive duplicates', () => {
+      const deduper = dedupe()
+      const source = [1, 1, 2, 2, 3, 3]
+      const result = deduper(source)
+      expect(into('array', result)).toEqual([1, 2, 3])
+    })
+
+    it('should get rid of any non-consecutive duplicates', () => {
+      const deduper = dedupe()
+      const source = [1, 2, 3, 1, 2, 3]
+      const result = deduper(source)
+      expect(into('array', result)).toEqual([1, 2, 3])
+    })
+  })
 })
